02064f8487d707b3a423b5ce1572dacf88716e32
Ewald Horn <ewaldhorn@gmail.com>
2019-08-02 15:40:32 +0200

    Set up insert activity. Linked to Firebase.

--- app/src/main/java/ewald/horn/alc4phase1challenge2/InsertActivity.java
+++ app/src/main/java/ewald/horn/alc4phase1challenge2/InsertActivity.java
@@ -3,11 +3,11 @@ package ewald.horn.alc4phase1challenge2;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class InsertActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_insert);
     }
 }
